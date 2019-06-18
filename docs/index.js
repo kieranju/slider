cba3f4fe6d644d44e1983e4dfa2550fa8884428c
Kieran Utz <kieranju@me.com>
2019-06-17 17:05:14 -0700

    Initial commit

---
+++ docs/index.js
@@ -0,0 +1,130 @@
+"use strict";
+var SlideOptions = (function () {
+    function SlideOptions() {
+        this.debug = false;
+        this.waitForImages = true;
+        this.tickTime = 300;
+        this.tockTime = 1000;
+        this.activeClassName = 'active';
+        this.loadedClassName = 'loaded';
+    }
+    return SlideOptions;
+}());
+var SlideHandler = (function () {
+    function SlideHandler(options) {
+        this.slides = [];
+        this.deckSelector = 'data-slider-deck';
+        this.cardSelector = 'data-slider-card';
+        this.options = options;
+        this.init();
+    }
+    SlideHandler.prototype.init = function () {
+        var _this = this;
+        var decks = Array.from(document.querySelectorAll("[" + this.deckSelector + "]"));
+        var cards = Array.from(document.querySelectorAll("[" + this.cardSelector + "]"));
+        var cardGroups = this.groupBy(cards, function (receiver) { return receiver.getAttribute(_this.cardSelector); });
+        decks.forEach(function (deck) {
+            var key = deck.getAttribute(_this.deckSelector);
+            var cardGroup = cardGroups.get(key);
+            _this.slides.push(new Slide(deck, cardGroup, _this.options));
+        });
+    };
+    SlideHandler.prototype.groupBy = function (items, callback) {
+        var itemGroups = new Map();
+        items.map(function (item) {
+            var key = callback(item);
+            var itemGroup = itemGroups.get(key);
+            if (!itemGroup) {
+                itemGroups.set(key, [item]);
+            }
+            else {
+                itemGroup.push(item);
+                itemGroups.set(key, itemGroup);
+            }
+        });
+        return itemGroups;
+    };
+    return SlideHandler;
+}());
+var Slide = (function () {
+    function Slide(deck, cards, options) {
+        var _this = this;
+        this.active = 0;
+        this.deck = deck;
+        this.cards = cards;
+        this.options = options;
+        if (this.options.waitForImages && window) {
+            window.onload = function () {
+                _this.init();
+            };
+        }
+        else {
+            this.init();
+        }
+    }
+    Slide.prototype.init = function () {
+        if (this.options.debug && !this.validate()) {
+            return;
+        }
+        this.start();
+    };
+    Slide.prototype.start = function () {
+        if (this.clock) {
+            return;
+        }
+        this.deck.classList.add(this.options.loadedClassName);
+        this.activate(this.active);
+        this.tick();
+    };
+    Slide.prototype.pause = function () {
+        if (!this.clock) {
+            return;
+        }
+        this.clock = clearInterval(this.clock);
+    };
+    Slide.prototype.tick = function () {
+        var _this = this;
+        this.clock = setInterval(function () { return _this.tock(); }, this.options.tockTime);
+    };
+    Slide.prototype.tock = function () {
+        var _this = this;
+        var currentActive = this.active;
+        var nextActive = (currentActive + 1);
+        if (nextActive === this.cards.length) {
+            this.active = 0;
+            this.deactivate(currentActive);
+            setTimeout(function () { return _this.activate(_this.active); }, this.options.tickTime);
+            return;
+        }
+        this.active = nextActive;
+        this.activate(nextActive);
+        setTimeout(function () { return _this.deactivate(currentActive); }, this.options.tickTime);
+    };
+    Slide.prototype.activate = function (index) {
+        this.cards[index].classList.add(this.options.activeClassName);
+    };
+    Slide.prototype.deactivate = function (index) {
+        this.cards[index].classList.remove(this.options.activeClassName);
+    };
+    Slide.prototype.validate = function () {
+        var _this = this;
+        if (!this.deck) {
+            console.error('Unable to target deck element.');
+            return false;
+        }
+        if (this.cards.length < 2) {
+            console.error('Not enough cards in the deck.');
+            return false;
+        }
+        var invalidCard = false;
+        this.cards.forEach(function (card, index) {
+            if (!card) {
+                invalidCard = true;
+                console.error("Unable to target card " + (index + 1) + " of " + _this.cards.length + ".", _this.cards);
+                return;
+            }
+        });
+        return !invalidCard;
+    };
+    return Slide;
+}());
---
+++ src/index.ts
@@ -0,0 +1,149 @@
+abstract class SlideOptions {
+    debug: boolean = false
+    waitForImages: boolean = true
+    tickTime: number = 300
+    tockTime: number = 1000
+    activeClassName: string = 'active'
+    loadedClassName: string = 'loaded'
+}
+
+class SlideHandler {
+    private options: SlideOptions
+    private slides: Slide[] = []
+    private deckSelector: string = 'data-slider-deck'
+    private cardSelector: string = 'data-slider-card'
+
+    constructor(options: SlideOptions) {
+        this.options = options
+        this.init()
+    }
+
+    private init() {
+        const decks = Array.from(document.querySelectorAll(`[${this.deckSelector}]`) as NodeListOf<HTMLElement>)
+        const cards = Array.from(document.querySelectorAll(`[${this.cardSelector}]`) as NodeListOf<HTMLElement>)
+        const cardGroups = this.groupBy(cards, receiver => receiver.getAttribute(this.cardSelector) as string)
+
+        decks.forEach((deck) => {
+            const key = deck.getAttribute(this.deckSelector) as string
+            const cardGroup = cardGroups.get(key) as HTMLElement[]
+            this.slides.push(new Slide(deck, cardGroup, this.options))
+        })
+    }
+
+    private groupBy(items: HTMLElement[], callback: (item: HTMLElement) => string): Map<string, HTMLElement[]> {
+        const itemGroups: Map<string, HTMLElement[]> = new Map()
+        items.map((item) => {
+            const key = callback(item)
+            const itemGroup = itemGroups.get(key)
+            if (!itemGroup) {
+                itemGroups.set(key, [item])
+            } else {
+                itemGroup.push(item)
+                itemGroups.set(key, itemGroup)
+            }
+        })
+
+        return itemGroups
+    }
+}
+
+class Slide {
+    private options: SlideOptions
+    private deck: HTMLElement
+    private cards: HTMLElement[]
+    private clock: any
+    private active: number = 0
+
+    constructor(deck: HTMLElement, cards: HTMLElement[], options: SlideOptions) {
+        this.deck = deck
+        this.cards = cards
+        this.options = options
+
+        if (this.options.waitForImages && window) {
+            window.onload = () => {
+                this.init()
+            }
+        } else {
+            this.init()
+        }
+    }
+
+    private init() {
+        if (this.options.debug && !this.validate()) {
+            return
+        }
+
+        this.start()
+    }
+
+    public start() {
+        if (this.clock) {
+            return
+        }
+
+        this.deck.classList.add(this.options.loadedClassName)
+        this.activate(this.active)
+        this.tick()
+    }
+
+    public pause() {
+        if (!this.clock) {
+            return
+        }
+
+        this.clock = clearInterval(this.clock)
+    }
+
+    private tick() {
+        this.clock = setInterval(() => this.tock(), this.options.tockTime)
+    }
+
+    private tock() {
+        let currentActive = this.active
+        let nextActive = (currentActive + 1)
+
+        if (nextActive === this.cards.length) {
+            // Cycle back to the first element, fade out the last element
+            this.active = 0
+            this.deactivate(currentActive)
+            setTimeout(() => this.activate(this.active), this.options.tickTime)
+            return
+        }
+
+        // Cycle to the next element, fade it in
+        this.active = nextActive
+        this.activate(nextActive)
+        setTimeout(() => this.deactivate(currentActive), this.options.tickTime)
+    }
+
+    private activate(index: number) {
+        this.cards[index].classList.add(this.options.activeClassName)
+    }
+
+    private deactivate(index: number) {
+        this.cards[index].classList.remove(this.options.activeClassName)
+    }
+
+    private validate(): boolean {
+        if (!this.deck) {
+            console.error('Unable to target deck element.')
+            return false
+        }
+
+        if (this.cards.length < 2) {
+            console.error('Not enough cards in the deck.')
+            return false
+        }
+
+        let invalidCard = false
+        this.cards.forEach((card, index) => {
+            if (!card) {
+                invalidCard = true
+                console.error(`Unable to target card ${index + 1} of ${this.cards.length}.`, this.cards)
+                return
+            }
+        })
+
+        return !invalidCard
+    }
+}
